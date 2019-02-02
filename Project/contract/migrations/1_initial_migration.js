b3a4bb4616982aea4d6a10822c07d2e23d4b637a
shivamnamdeo43 <43555424+shivamnamdeo43@users.noreply.github.com>
2019-02-02 17:52:56 -0500

    Add files via upload

---
+++ Project/contract/migrations/1_initial_migration.js
@@ -0,0 +1,7 @@
+var Migrations = artifacts.require("/Users/shivamnamdeo/Desktop/Project/contract/contracts/Migrations.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+};
+
+
